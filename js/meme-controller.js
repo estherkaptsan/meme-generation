a7d09a0fa31671d4df9a6f587f0c782e6a29ac79
esther kaptsan <estherkaptsan@gmail.com>
2023-02-15 16:48:21 +0200

    my first commit

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,9 +1,103 @@
 'use strict'
 
+const TOUCH_EVS = ['touchstart', 'touchmove', 'touchend']
+
+let gElCanvas
+let gCtx
+let gText
+
 function onInit() {
-    console.log('first commmit') 
+    gElCanvas = document.querySelector('#my-canvas')
+    gCtx = gElCanvas.getContext('2d')
+
+    createMeme()
+
+    addListeners()
+
+    getTextInformation()
+    draw()
+
+    renderGallery()
+}
+
+function renderGallery() {
+    let strHtmls = gImgs.map(img => `
+        <img src="images/${img.id}.jpg" >
+    `)
+    document.querySelector('.gallery-container').innerHTML = strHtmls.join('')
 }
 
 function renderMeme() {
+    let meme = getMeme()
+    console.log(meme.selectedImgId)
+}
+
+function draw() {
+    drawImg()
+    // const textInfo = getTextInformation()
+    // const {txt, fontSize, color} = getTextInformation()
+    // drawText(30, 30, fontSize, color, txt)
+}
+
+function drawImg() {
+    const img = new Image()
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+    }
+    const { selectedImgId } = getMeme()
+    img.src = gImgs[selectedImgId].url
+}
+
+function getTextInformation() {
+    const txt = document.querySelector('.txt').value
+    const color = document.querySelector('.color').value
+    const fontSize = document.querySelector('.font-size').value
+    drawText(30, 40, fontSize, color, txt)
+
+    // return { txt, fontSize, color }
+}
+
+function drawText(x, y, size, color, txt) {
+    gCtx.lineWidth = 1
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = color
+    gCtx.font = `${size}px Impact`
+    gCtx.textAlign = 'center'
+    gCtx.textBaseline = 'middle'
+
+    gCtx.fillText(txt, x, y)
+    gCtx.strokeText(txt, x, y)
+}
+//-----------------------------------------------------------------------------------------------------------
+function addListeners() {
+    addMouseListeners()
+    addTouchListeners()
+}
+
+function addMouseListeners() {
+    gElCanvas.addEventListener('mousedown', onDown)
+    gElCanvas.addEventListener('mousemove', onMove)
+    gElCanvas.addEventListener('mouseup', onUp)
+}
+
+function addTouchListeners() {
+    gElCanvas.addEventListener('touchstart', onDown)
+    gElCanvas.addEventListener('touchmove', onMove)
+    gElCanvas.addEventListener('touchend', onUp)
+}
+
+function onDown() {
+    console.log('Down')
+
+}
+
+function onMove() {
+    console.log('Move')
+    // draw()
+    renderMeme()
+}
+
+function onUp() {
+    console.log('Up')
 
 }
--- js/services/meme-service.js
+++ js/services/meme-service.js
@@ -1,14 +1,54 @@
 'use strict'
- 
+
 let gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
-let gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
+let gImgs = [
+    { id: 1, url: 'images/1.jpg', keywords: ['funny', 'cat'] },
+    { id: 2, url: 'images/2.jpg', keywords: ['funny', 'cat'] },
+    { id: 3, url: 'images/3.jpg', keywords: ['funny', 'cat'] },
+    { id: 4, url: 'images/4.jpg', keywords: ['funny', 'cat'] },
+    { id: 5, url: 'images/5.jpg', keywords: ['funny', 'cat'] },
+    { id: 6, url: 'images/6.jpg', keywords: ['funny', 'cat'] },
+    { id: 7, url: 'images/7.jpg', keywords: ['funny', 'cat'] },
+    { id: 8, url: 'images/8.jpg', keywords: ['funny', 'cat'] },
+    { id: 9, url: 'images/9.jpg', keywords: ['funny', 'cat'] },
+    { id: 10, url: 'images/10.jpg', keywords: ['funny', 'cat'] },
+]
+
 let gMeme = {
-    selectedImgId: 5,
+    selectedImgId: 1,
     selectedLineIdx: 0,
     lines: [
         {
             txt: 'I sometimes eat Falafel',
-            size: 20, align: 'left',
+            size: 20,
+            align: 'left',
             color: 'red'
         }]
+}
+
+function createMeme() {
+    gMeme = {
+        selectedImgId: 2, // select 1 img from gallery
+        selectedLineIdx: 0,
+        lines: [
+            {
+                txt: document.querySelector('.txt').value,
+                size: document.querySelector('.font-size').valuev,
+                align: 'left',
+                color: document.querySelector('.color').value,
+            }
+        ]
+    }
+}
+
+function getMeme() {
+    return gMeme
+}
+
+function getSizeText() {
+    return gMeme.lines[0].size = document.querySelector('.font-size').value
+}
+
+function getColorText() {
+    return gMeme.lines[0].color = document.querySelector('.color').value
 }
